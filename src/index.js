eac3fe396e984385a2881659f8b2e40c142a3f56
LMDEHJULIO <juliodevelops@gmail.com>
2017-11-05 11:50:08 -0500

    Fix empty video upon rendering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyDZiq4HPyVgeFGAg9oRwuvogKmqHshb7gY';
 
 
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail />
         <VideoList videos={this.state.videos}/>
       </div>
     )
