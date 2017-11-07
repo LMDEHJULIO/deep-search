99e7743186d5f219d4f9541705a7b973d095d429
LMDEHJULIO <juliodevelops@gmail.com>
2017-11-06 20:40:02 -0500

    test video option

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     )
