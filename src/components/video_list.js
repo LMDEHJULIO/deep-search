48f33a282305d1cbb1a8460ba37b6fa7c979ddd0
LMDEHJULIO <juliodevelops@gmail.com>
2017-11-07 04:35:22 -0500

    VideoSelect functionality working

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,12 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} key={video.etag}/> // etag is a property from the Ysearch API
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        video={video}
+        key={video.etag}/> // etag is a property from the Ysearch API
+    )
   });
 
   return(
